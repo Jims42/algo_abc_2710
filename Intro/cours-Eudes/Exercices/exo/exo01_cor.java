011404bb26f14ca0889c9ead2d1f590e530bce11
Jims42 <jimmycoco.42@outlook.fr>
2025-12-04 16:43:44 +0100

    boucle

---
+++ Intro/cours-Eudes/Exercices/exo/exo01_cor.java
@@ -0,0 +1,33 @@
+package exo;
+
+ import java.util.Scanner;3
+
+public class exo01_cor {
+
+    public static void main(String[] args) {
+        
+
+
+        int a;
+        int b;
+        int addition;
+
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.println("la valeur de a");
+        a = clavier.nextInt();
+        System.out.println("la valeur de b");
+        b = clavier.nextInt();
+
+        addition = a+b;
+
+        System.out.println("a+b= "+addition);
+
+        clavier.close();
+
+
+
+    }
+    
+
+}
