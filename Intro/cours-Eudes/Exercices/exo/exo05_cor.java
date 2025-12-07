8d020d8d1ced2870742c1f593dd2e8fd11b6c188
Jims42 <jimmycoco.42@outlook.fr>
2025-12-07 12:54:29 +0100

    exo

--- Intro/cours-Eudes/Exercices/exo/exo05_cor.java
+++ Intro/cours-Eudes/Exercices/exo/exo05_cor.java
@@ -6,23 +6,24 @@ public class exo05_cor {
 
     public static void main(String[] args) {
 
-        double a;
-        double b;
-        double c;
-        double equation;
+        int a;
+        int b;
+        int c;
+        int equation;
   
         Scanner clavier = new Scanner(System.in);
 
-System.out.println("valeur de a = ");
-clavier.nextDouble();
+System.out.println("valeur de a");
+a = clavier.nextInt();
 
-System.out.println("valeur de b = ");
-clavier.nextDouble();
+System.out.println("valeur de b");
+b = clavier.nextInt();
 
-System.out.println("valeur de c = ");
-clavier.nextDouble();
+System.out.println("valeur de c");
+c = clavier.nextInt();
 
 equation = a*b/c;
+
 
 System.out.println(" a x b / c = "+equation);
 
@@ -30,11 +31,12 @@ if (equation>10) {
     System.out.println("la valeur est superieur a 10 ");
     
     
-}if (equation==10) {
+}else if (equation==10) {
     
     System.out.println(" la valeur est egal a 10");
 
-} else {
+} 
+else {
 
     System.out.println("la valeur est inferieur a 10");
 
