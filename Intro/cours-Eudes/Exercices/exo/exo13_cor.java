fbba6a5b7835563061c54376b82beec7aea99008
Jims42 <jimmycoco.42@outlook.fr>
2025-12-10 12:46:16 +0100

    exo

---
+++ Intro/cours-Eudes/Exercices/exo/exo13_cor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class exo13_cor {
+
+    public static void main(String[] args) {
+        
+        int cm ;
+        double m ;
+
+        Scanner clavier = new Scanner(System.in);
+        
+        
+
+        System.out.println("Donner une taille en centimètre ? ");
+        cm = clavier.nextInt();
+        m = cm / 100.0;
+
+        System.out.println(cm+" cm vaut "+m+" mètres");
+
+clavier.close();
+
+    }
+
+    
+}
