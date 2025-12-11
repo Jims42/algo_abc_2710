3d460b6da16df74530d2688e26611eeef40426e0
Jims42 <jimmycoco.42@outlook.fr>
2025-12-11 10:19:54 +0100

    ex

--- Intro/cours-Eudes/Exercices/exo/exo15_cor.java
+++ Intro/cours-Eudes/Exercices/exo/exo15_cor.java
@@ -4,23 +4,33 @@ public class exo15_cor {
 
     public static void main(String[] args) {
         
-        String devise1;
-        String devise2;
+        String EUR;String USD;String GPD;
+        String  resultat1;String resultat2;String resultat3;
+        String reponse;String defaut; 
+        
+        
+
 
         Scanner clavier = new Scanner(System.in);
-
+       defaut=clavier.next();
+        while (defaut) {
+            
         
         System.out.println("Quel est votre monaie ? ");
-        clavier.next();
-        if ( devise1.equals("USD")||devise1.equals("GBP")){
-
-            
-        } 
-            
+        reponse=clavier.next();
+        if (reponse.equalsIgnoreCase("EUR")) { resultat1="Quel est votre montant ?";}
+        if (reponse.equalsIgnoreCase("USD")) {resultat2="Quel est votre montant ?";}
+        if (reponse.equalsIgnoreCase("GPD")) {resultat3="Quel est votre montant ?";}
+        else{.equals(defaut);
+            System.out.println("Veillez choisir entre: EUR ou USD ou GPD");
         }
+
+        }
+
 
 
 
     }
     
+
 }
