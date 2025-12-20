a76e55993722f254339e79bb1be4008c893b8e74
Jims42 <jimmycoco.42@outlook.fr>
2025-12-20 10:38:33 +0100

    exo

---
+++ Intro/essai/Exo01.java
@@ -0,0 +1,11 @@
+package Intro.essai;
+
+import java.util.Scanner;
+
+public class Exo01 {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        
+    }
+    
+}
