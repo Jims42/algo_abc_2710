618f2531922423887dd96b0215aa1273ff21a99f
Jims42 <jimmycoco.42@outlook.fr>
2025-12-21 10:22:07 +0100

    test

---
+++ Intro/essai/Test.js
@@ -0,0 +1,14 @@
+
+    //   const btn1 = document.getElementById("btn1");
+    //   const btn2 = document.getElementById("btn2");
+    //   const btn3 = document.getElementById("btn3");
+    //   const btn4 = document.getElementById("btn4");
+    //   const btn5 = document.getElementById("btn5");
+      
+      btn1.onclick = () => document.body.style.backgroundColor = "blue";
+      btn2.onclick = () => document.body.style.backgroundColor = "violet";
+      btn3.onclick = () => document.body.style.backgroundColor = "green";
+      btn4.onclick = () => document.body.style.backgroundColor = "yellow";
+      btn5.onclick = () => document.body.style.backgroundColor = "orange";
+      
+    
