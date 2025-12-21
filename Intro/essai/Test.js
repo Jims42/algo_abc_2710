61ec46e9ae8a1fffc1f5f930e4edd1d0410871dc
Jims42 <jimmycoco.42@outlook.fr>
2025-12-21 10:44:21 +0100

    test

--- Intro/essai/Test.js
+++ Intro/essai/Test.js
@@ -4,11 +4,13 @@
     //   const btn3 = document.getElementById("btn3");
     //   const btn4 = document.getElementById("btn4");
     //   const btn5 = document.getElementById("btn5");
+    //   const btn6 = document.getElementById("btn6");
       
       btn1.onclick = () => document.body.style.backgroundColor = "blue";
       btn2.onclick = () => document.body.style.backgroundColor = "violet";
       btn3.onclick = () => document.body.style.backgroundColor = "green";
       btn4.onclick = () => document.body.style.backgroundColor = "yellow";
       btn5.onclick = () => document.body.style.backgroundColor = "orange";
+      btn6.onclick = () => document.body.style.backgroundColor = "red";
       
     
