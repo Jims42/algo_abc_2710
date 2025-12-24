5b2801e611a7eb22b7c9f75b24e2264a703ebc69
Jims42 <jimmycoco.42@outlook.fr>
2025-12-24 08:11:26 +0100

    cor

--- Intro/cours_Franck/exercises/Exo_03_4_correction.java
+++ Intro/cours_Franck/exercises/Exo_03_4_correction.java
@@ -1,64 +1,82 @@
 import java.util.Scanner;
 
 public class Exo_03_4_correction {
-    public static void main(String[] args) {
-        Scanner clavier=new Scanner(System.in);
-        boolean trouvepoint= false;
-        boolean trouvelettre=false;
 
-        int nbchar=0;
-        int [] occurrence= new int[nbchar];
-        String mot;
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        boolean trouvePoint = false;
+        boolean trouveLettre = false;
+
+        int[] occurence;
+
         char lettre;
 
         String chaineString;
 
-        do{
-            System.out.println("Veuillez saisir unr phrase terminée par un point ! ");
-            chaineString=clavier.nextLine();
-            int depart=chaineString.length()-1;
-            String souString=chaineString.substring(depart);
-            if(souString.equals(".")){
-                trouvepoint=true;
+        do {
+            System.out.println(" Veuillez saisir une phrase terminée par un point !");
+            chaineString = sc.nextLine().toLowerCase();
+            int depart = chaineString.length() - 1;
+            String souString = chaineString.substring(depart);
+            if (souString.equals(".")) {
+                trouvePoint = true;
             }
 
+        } while (trouvePoint == false);
 
-        }while(trouvepoint==false);
-        chaineString=chaineString.replace("", "");
-        //chaineString=chaineString.replace("é", "e");
-        System.out.println("veuillez saisir une lettre qui sera peut-être dans la phrase : ");
-        lettre=clavier.next().charAt(0);
+        chaineString = chaineString.replace(" ", "");
+        // String newchaineString = chaineString.replace( "à", "a");
 
-        
+        System.out.println(chaineString);
+        occurence = new int[chaineString.length()];
+
+        System.out.println(" Veuillez saisir une lettre qui sera peut-être dans la phrase :");
+        lettre = sc.nextLine().charAt(0);
+
         // String chainetab="";
-        char[ ]tabphrase=new char[chaineString.length()];
 
-        for (int i=0 ; i<chaineString.length();i++) {
-             
-            tabphrase[i]=chaineString.charAt(i);
+        char[] tabphrase = new char[chaineString.length()];
+
+        for (int i = 0; i < chaineString.length(); i++) {
+
+            tabphrase[i] = chaineString.charAt(i);
         }
-        System.out.println("[ ");
+
+        System.out.print("[ ");
         for (char c : tabphrase) {
-            System.out.println(c+" ");
+            System.out.print(c + " ");
         }
-        System.out.println("] ");
-        int position=0;
+
+        System.out.print("] ");
+        int position = 0;
 
         for (int i = 0; i < tabphrase.length; i++) {
-            if (tabphrase[i]==lettre) {
-                occurrence[position]=i+1;
-                trouvelettre=true;
+            if (tabphrase[i] == lettre) {
+                occurence[position] = i + 1;
+
+                trouveLettre = true;
+
                 position++;
+
             }
-            if (trouvelettre==false) {
-                System.out.println("La lettre n'est pas présente dans la phrase");
-            }else{
-                System.out.println("La lettre est présente dans le tableau de char aux positions suivante : ");
-                for (int value  : occurrence) {
-                    
-                }
-            }
+
         }
+
+        if (trouveLettre == false) {
+            System.out.println("la lettre n'est pas présente  dans la phrase");
+        } else {
+
+            System.out.println(" la lettre est présente dans le tableau de char aux positions suivantes :");
+            for (int value : occurence) {
+                if (value != 0) {
+                    System.out.print(value + " ");
+
+                }
+
+            }
+
+        }
+        sc.close();
     }
-    
 }
+ 
