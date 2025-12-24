636d112e301cfb78b6f940fc3b7bfcc3d399b7fd
Jims42 <jimmycoco.42@outlook.fr>
2025-12-24 11:59:50 +0100

    ex

--- Intro/essai/Exo01.java
+++ Intro/essai/Exo01.java
@@ -22,6 +22,28 @@ public class Exo01 {
         System.out.println(i);
         
     }
+    String[] tableau=new String[7];
+    tableau[0]="\u001B[30mGris\u001B[0m";
+    tableau[1]="\u001B[31mRouge\u001B[0m";
+    tableau[2]="\u001B[32mVert\u001B[0m";
+    tableau[3]="\u001B[33mJaune\u001B[0m";
+    tableau[4]="\u001B[34mBleu\u001B[0m";
+    tableau[5]="\u001B[35mViolet\u001B[0m";
+    tableau[6]="\u001B[36mBleu clair\u001B[0m";
+
+    
+
+    
+    for (String couleur : tableau) {
+        System.out.print(couleur+"\t");
+    }System.out.println();
+
+    String[] tableau1=new String[]{"\u001B[30mGris\u001B[0m","\u001B[31mRouge\u001B[0m","\u001B[32mVert\u001B[0m","\u001B[33mJaune\u001B[0m","\u001B[34mBleu\u001B[0m","\u001B[35mViolet\u001B[0m","\u001B[36mBleu clair\u001B[0m"};
+
+    for (String couleur1 : tableau1) {
+        System.out.println(couleur1);
+    }
+
   clavier.close();  
 }
     
