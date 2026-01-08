4fbc225c14aa155435475c1890781a09631a0a08
Jims42 <jimmycoco.42@outlook.fr>
2026-01-08 17:57:04 +0100

    pendu

--- Intro/cours_Franck/exercises/Exercice_Inter/LePendu.java
+++ Intro/cours_Franck/exercises/Exercice_Inter/LePendu.java
@@ -6,47 +6,43 @@ import java.io.Console;
 public class LePendu {
     public static void main(String[] args) {
         // Scanner clavier = new Scanner(System.in);
-        Console console=System.console();
-        String mot=" " ;
-        String motCherche="";
+        Console console = System.console();
+        String mot = " ";
+        String motCherche = "";
         while (mot.length() <= 5) {
-        //     System.out.println("Entrer un mot de plus de 5 caractère");
-        //     mot = clavier.nextLine().toUpperCase();
-            
-            char[] motSecret=console.readPassword("Entrer un mot de plus de 5 caractère");
-            String mots=new String(motSecret);
+            // System.out.println("Entrer un mot de plus de 5 caractère");
+            // mot = clavier.nextLine().toUpperCase();
+
+            char[] motSecret = console.readPassword("Entrer un mot de plus de 5 caractère");
+            mot = new String(motSecret).toUpperCase();
             java.util.Arrays.fill(motSecret, ' ');
-            
-           
 
-            System.out.println("Vous avez entré : "+mots);
-             mots= mots.toUpperCase();
-             System.out.println(mots);
+            // System.out.println("Vous avez entré : "+mot);
 
-            if (mots.length() <= 5) {
+            if (mot.length() <= 5) {
                 System.out.println("Mot trop court ! recommencer \n ");
             }
-        
+
         }
 
-        char[] affichage=new char[mot.length()];
+        char[] affichage = new char[mot.length()];
 
         for (int i = 0; i < mot.length(); i++) {
-            if (i==0||i==mot.length()-1) {
-                affichage[i]=mot.charAt(i);
-            }else{
-                affichage[i]='_';
+            if (i == 0 || i == mot.length() - 1) {
+                affichage[i] = mot.charAt(i);
+            } else {
+                affichage[i] = '_';
             }
         }
-// System.out.println(affichage);
-     
-//        for (char c : affichage) {
-//         motCherche+=c+" ";
-//        }
-       
-// System.out.println(motCherche);
+        // System.out.println(affichage);
+
+        for (char c : affichage) {
+            motCherche += c + " ";
+        }
+
+        System.out.println(motCherche);
         // clavier.close();
-        
+
     }
 
 }
