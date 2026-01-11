213397638822ec34addc897db8b97b65ef85f72c
Jims42 <jimmycoco.42@outlook.fr>
2026-01-11 15:50:23 +0100

    pendu

--- Intro/cours_Franck/exercises/Exercice_Inter/LePendu.java
+++ Intro/cours_Franck/exercises/Exercice_Inter/LePendu.java
@@ -10,14 +10,10 @@ public class LePendu {
         String mot = " ";
         String motCherche = "";
         while (mot.length() <= 5) {
-            // System.out.println("Entrer un mot de plus de 5 caractère");
-            // mot = clavier.nextLine().toUpperCase();
 
             char[] motSecret = console.readPassword("Entrer un mot de plus de 5 caractère");
             mot = new String(motSecret).toUpperCase();
             java.util.Arrays.fill(motSecret, ' ');
-
-            // System.out.println("Vous avez entré : "+mot);
 
             if (mot.length() <= 5) {
                 System.out.println("Mot trop court ! recommencer \n ");
@@ -26,8 +22,10 @@ public class LePendu {
         }
 
         char[] affichage = new char[mot.length()];
+        char[] affichageFinal = new char[mot.length()];
 
         for (int i = 0; i < mot.length(); i++) {
+            affichageFinal[i] = mot.charAt(i);
             if (i == 0 || i == mot.length() - 1) {
                 affichage[i] = mot.charAt(i);
             } else {
@@ -38,7 +36,8 @@ public class LePendu {
         String choixLettre = " ";
         int nbEssai = 6;
         boolean motTrouve = false;
-        String tabLettreFausse = "";
+        char[] tabLettreFausse = new char[6];
+        int nbLettreFausse = 0;
 
         while (nbEssai > 0 && !motTrouve) {
             motCherche = "";
@@ -49,14 +48,34 @@ public class LePendu {
             System.out.println("<<< TROUVER LE MOT SUIVANT >>>\n\t" + new String(motCherche) + "\n");
 
             System.out.print("<<< NOMBRE DE TENTATIVE(S) RESTANTE >>>\n\t\t \u001B[4;33m" + nbEssai + "\u001B[4;0m \n");
+            if (nbLettreFausse > 0) {
+                System.out.print("=== Lettre incorect déja utilisé >>>> : ");
+                for (int i = 0; i < nbLettreFausse; i++) {
+                    System.out.print("\u001B[31m" + tabLettreFausse[i] + "\u001B[0m  ");
+                }
+                System.out.println();
+            }
             System.out.print("<<< ENTRER UNE LETTRE >>> \n\t");
             choixLettre = clavier.nextLine().toUpperCase();
 
             if (choixLettre.length() != 1) {
                 System.out.println("Entrer une seule lettre !");
+                continue;
             }
             char lettre = choixLettre.charAt(0);
             boolean lettreTrouve = false;
+            boolean lettreDouble = false;
+
+            for (int i = 0; i < nbLettreFausse; i++) {
+                if (tabLettreFausse[i] == lettre) {
+                    lettreDouble = true;
+                    break;
+                }
+            }
+            if (lettreDouble) {
+                System.out.println("===> \u001B[31mVous avez déjà proposé cette lettre\u001B[0m <===] ");
+                continue;
+            }
 
             for (int i = 0; i < mot.length(); i++) {
                 if (mot.charAt(i) == lettre) {
@@ -66,18 +85,43 @@ public class LePendu {
                 }
             }
             if (!lettreTrouve) {
+                tabLettreFausse[nbLettreFausse] = lettre;
+                nbLettreFausse++;
                 System.out.println(
                         "La lettre \u001B[31m" + lettre + "\u001B[0m n'est pas dans le mot,\n vous perdez 1 essai");
                 nbEssai--;
-                char[] lettreFausse = new char[lettre];
 
-                for (char n : lettreFausse) {
-                    tabLettreFausse += n + " ";
-                    
+            }
+            motTrouve = true;
+            for (char c : affichage) {
+                if (c == '_') {
+                    motTrouve = false;
+                    break;
                 }
             }
 
         }
+        String motFinal = "";
+
+        for (char c : affichageFinal) {
+            motFinal += c + " ";
+        }
+        if (motTrouve) {
+            System.out.println(
+                    "\t\t\u001B[4;32m<<< F E L I C I T A T I O N >>>\u001B[4;0m\nLe mot caché était bien >>> \u001B[32m"
+                            + motFinal + "\u001b[0m");
+        } else {
+            System.out.println("\t\t\u001B[4;35m<<< D O M M A G E >>>\u001B[4;0m\nLe mot caché était >>> \u001B[35m"
+                    + motFinal + "\u001B[0m");
+        }
+        if (nbLettreFausse > 0) {
+            System.out.print("Les lettres incorrectes utilisées sont >>> ");
+            for (int i = 0; i < nbLettreFausse; i++) {
+                System.out.print("\u001B[31m" + tabLettreFausse[i] + "\u001B[0m  ");
+            }
+            System.out.println();
+        }
+        System.out.println("MERCI POUR VOTRE PARTICIPATION");
 
         clavier.close();
 
