00c69599c8163249151c2fb4c89493409b123cf6
Jims42 <jimmycoco.42@outlook.fr>
2026-01-14 09:13:36 +0100

    tab

--- Intro/cours_Franck/exercises/Exercice_Inter/Dichotomie.java
+++ Intro/cours_Franck/exercises/Exercice_Inter/Dichotomie.java
@@ -1,69 +1,63 @@
 package Exercice_Inter;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Scanner;
 
 public class Dichotomie {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
 
-        // // String[] tabNoms = new String[7];
-        // // for (int i = 0; i < tabNoms.length; i++) {
-        // // System.out.print("Entrer le Nom dans la case "+(i+1)+" : ");
-        // // String prenoms=clavier.nextLine().toUpperCase();
-        // // }
-        // Collection.sort(tabNoms);
-        // for (int i = 0; i < tabNoms.length; i++) {
-        //     tabNoms[i]=(String) tabNoms[i].toUpperCase();
-        //     System.out.println(i + 1 + " - || " + tabNoms[i] + " ||");
-        // }
-
-
-
-        String[] tabNoms = new String[] { "agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie" };
-        
+        System.out.println("Combien de noms voulez-vous entrer ?");
+        int n = clavier.nextInt();
+        clavier.nextLine();
+        String[] tabNoms = new String[n];
         for (int i = 0; i < tabNoms.length; i++) {
-            tabNoms[i]=(String) tabNoms[i].toUpperCase();
+            System.out.print("Entrer le Nom dans la case " + (i + 1) + " : ");
+            tabNoms[i] = clavier.nextLine().toUpperCase();
+        }
+        Arrays.sort(tabNoms);
+        for (int i = 0; i < tabNoms.length; i++) {
+            tabNoms[i] = (String) tabNoms[i].toUpperCase();
             System.out.println(i + 1 + " - || " + tabNoms[i] + " ||");
         }
+
+        // String[] tabNoms = new String[] { "agathe", "berthe", "chloé", "cunégonde",
+        // "olga", "raymonde", "sidonie" };
+
+        // for (int i = 0; i < tabNoms.length; i++) {
+        // tabNoms[i]=(String) tabNoms[i].toUpperCase();
+        // System.out.println(i + 1 + " - || " + tabNoms[i] + " ||");
+        // }
         System.out.println("Entrer le prénom recherché : ");
-        String nomRecherche=clavier.nextLine();
-        
-        int resultat=chercheprenom(tabNoms, nomRecherche);
-        if (resultat !=-1) {
-            System.out.println("Prénom trouvé à l'indice : "+resultat);
-        }else{
+        String nomRecherche = clavier.nextLine().toUpperCase();
+
+        int resultat = chercheprenom(tabNoms, nomRecherche);
+        if (resultat != -1) {
+            System.out.println("Prénom trouvé à l'indice : " + resultat);
+        } else {
             System.out.println("Prénom nom trouvé");
         }
-        
 
         clavier.close();
     }
-    public static int chercheprenom(String[] tabNoms, String nomRecherche){
-        int gauche =0;
-        int droite =tabNoms.length -1;
 
-        while (gauche<=droite) {
-            int milieu = gauche +(droite - gauche)/2;
+    public static int chercheprenom(String[] tabNoms, String nomRecherche) {
+        int gauche = 0;
+        int droite = tabNoms.length - 1;
+
+        while (gauche <= droite) {
+            int milieu = gauche + (droite - gauche) / 2;
 
             int comparaison = nomRecherche.compareTo(tabNoms[milieu]);
-            if (comparaison==0) {
+            if (comparaison == 0) {
                 return milieu;
-            }else if (comparaison<0) {
-                droite=milieu-1;
-            }else {
-                gauche=milieu+1;
+            } else if (comparaison < 0) {
+                droite = milieu - 1;
+            } else {
+                gauche = milieu + 1;
             }
         }
         return -1;
     }
-    
 
-
-
-    
-    
-        
-    }
-
+}
