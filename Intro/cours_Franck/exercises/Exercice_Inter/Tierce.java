22a033387780ed5864b82d9d9c063f1f1900eebd
Jims42 <jimmycoco.42@outlook.fr>
2026-01-19 16:00:21 +0100

    rt

--- Intro/cours_Franck/exercises/Exercice_Inter/Tierce.java
+++ Intro/cours_Franck/exercises/Exercice_Inter/Tierce.java
@@ -12,41 +12,56 @@ public class Tierce {
         System.out.println("Indiquer le nombre de chevaux que vous avez joués :");
         int nbJouer = clavier.nextInt();
         // Utilisation de BigInteger pour les calculs
-        BigInteger factN = bigcalculerFactorielle(nbParticipant);
-        BigInteger factP = bigcalculerFactorielle(nbJouer);
-        BigInteger factNMoisP = bigcalculerFactorielle(nbParticipant - nbJouer);
+        // BigInteger factN = bigcalculerFactorielle(nbParticipant);
+        // BigInteger factP = bigcalculerFactorielle(nbJouer);
+        // BigInteger factNMoisP = bigcalculerFactorielle(nbParticipant - nbJouer);
 
-        BigInteger resultatX = factN.divide(factNMoisP);
-        BigInteger resultatY = factN.divide(factP.multiply(factNMoisP));
+        // BigInteger resultatX = factN.divide(factNMoisP);
+        // BigInteger resultatY = factN.divide(factP.multiply(factNMoisP));
 
-         System.out.println("------- <<< RESULTAT >>> -------\n");
-        System.out.printf(Locale.US, "Dans l'ordre : 1 Chance sur %,d%n ", resultatX);
+        //  System.out.println("------- <<< RESULTAT >>> -------\n");
+        // System.out.printf(Locale.US, "Dans l'ordre : 1 Chance sur %,d%n ", resultatX);
       
-        System.out.println("\t--------");
-        System.out.printf(Locale.US, "Dans le désordre : 1 chance sur %,d%n : ", resultatY);
-
-        System.out.println(
-                "Les jeux d'argent et de hasard peuvent être dangereux : pertes d'argent, conflits familiaux, addiction…\n Retrouvez nos conseils sur joueurs-info-service.fr (09 74 75 13 13 - appel non surtaxé)");
-
-        clavier.close();
-
-        // utilisation avec maximum 20 participats(long max 64 bits)
-
-        // // long resultatX = calculerFactorielle(nbParticipant) / calculerFactorielle(nbParticipant - nbJouer);
-        // // long resultatY = calculerFactorielle(nbParticipant)
-        // //         / (calculerFactorielle(nbJouer) * calculerFactorielle(nbParticipant - nbJouer));
-
-        // System.out.println("------- <<< RESULTAT >>> -------\n");
-        // // System.out.printf(Locale.US, "Dans l'ordre : 1 Chance sur %,d%n ", resultatX);
-        
         // System.out.println("\t--------");
-        // // System.out.printf(Locale.US, "Dans le désordre : 1 chance sur %,d%n : ", resultatY);
+        // System.out.printf(Locale.US, "Dans le désordre : 1 chance sur %,d%n : ", resultatY);
 
         // System.out.println(
         //         "Les jeux d'argent et de hasard peuvent être dangereux : pertes d'argent, conflits familiaux, addiction…\n Retrouvez nos conseils sur joueurs-info-service.fr (09 74 75 13 13 - appel non surtaxé)");
 
         // clavier.close();
-    }
+
+        // utilisation avec maximum 20 participats(long max 64 bits)
+
+    //     long resultatX = calculerFactorielle(nbParticipant) / calculerFactorielle(nbParticipant - nbJouer);
+    //     long resultatY = calculerFactorielle(nbParticipant)
+    //             / (calculerFactorielle(nbJouer) * calculerFactorielle(nbParticipant - nbJouer));
+
+    //     System.out.println("------- <<< RESULTAT >>> -------\n");
+    //     System.out.printf(Locale.US, "Dans l'ordre : 1 Chance sur %,d%n ", resultatX);
+        
+    //     System.out.println("\t--------");
+    //     System.out.printf(Locale.US, "Dans le désordre : 1 chance sur %,d%n : ", resultatY);
+
+    //     System.out.println(
+    //             "Les jeux d'argent et de hasard peuvent être dangereux : pertes d'argent, conflits familiaux, addiction…\n Retrouvez nos conseils sur joueurs-info-service.fr (09 74 75 13 13 - appel non surtaxé)");
+
+    //     clavier.close();
+    // }
+    long resultatX = factorielleRecursive(nbParticipant) / factorielleRecursive(nbParticipant - nbJouer);
+        long resultatY = factorielleRecursive(nbParticipant)
+                / (factorielleRecursive(nbJouer) * factorielleRecursive(nbParticipant - nbJouer));
+
+        System.out.println("------- <<< RESULTAT >>> -------\n");
+        System.out.printf(Locale.US, "Dans l'ordre : 1 Chance sur %,d%n ", resultatX);
+        
+        System.out.println("\t--------");
+        System.out.printf(Locale.US, "Dans le désordre : 1 chance sur %,d%n : ", resultatY);
+
+        System.out.println(
+                "Les jeux d'argent et de hasard peuvent être dangereux : pertes d'argent, conflits familiaux, addiction\n Retrouvez nos conseils sur joueurs-info-service.fr (09 74 75 13 13 - appel non surtaxé)");
+
+        clavier.close();
+}
 
     public static long calculerFactorielle(int n) {
         long f = 1;
@@ -61,5 +76,20 @@ public class Tierce {
             f = f.multiply(BigInteger.valueOf(i));
         }
         return f;
+    }
+    public static long calculerFactorielleOO(long n) {
+        long f = 1;
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        return f;
+    }
+    public static long factorielleRecursive(long _nb){
+        if (_nb>1) {
+
+          return  _nb*factorielleRecursive(_nb-1);
+        } else {
+            return 1;
+        }
     }
 }
