b62243b11d2b993b13669be3a4dd40c466c6a586
Jims42 <jimmycoco.42@outlook.fr>
2026-02-16 08:54:49 +0100

    course

--- cours_Franck/Objets/appVoiture/src/VoitureDeCourse.java
+++ cours_Franck/Objets/appVoiture/src/VoitureDeCourse.java
@@ -1,29 +1,33 @@
 public class VoitureDeCourse extends Voiture {
 
-    public VoitureDeCourse(){
-        this.marqueVoiture="";
-        this.poids=0;
-    
+    public VoitureDeCourse() {
+        this.marqueVoiture = "";
+        this.poids = 0;
+
     }
-    public VoitureDeCourse(String _marque, int _poids, Moteur _moteur){
-        this.marqueVoiture=_marque;
-        this.poids=_poids;
+
+    public VoitureDeCourse(String _marque, int _poids, Moteur _moteur) {
+        this.marqueVoiture = _marque;
+        this.poids = _poids;
 
         try {
-            this.moteur=_moteur;
+            this.moteur = _moteur;
             if (!this.moteur.getMarqueMoteur().equals(this.marqueVoiture)) {
                 throw new Exception("Le moteur et la marque de la voiture de course ne sont pas compatible");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
 
-        } 
         }
-        public double vitesseMaxF1(){
-		double vitesseMaxF1=this.moteur.getVitesseTheorique()-(this.poids*0.05);
-		return vitesseMaxF1;
-	}
-        public String toString(){
-          return "------------------------VOITURE DE COURSE---------------------------------------------\nMarque ==> "+marqueVoiture+"\nPoids ==> "+poids+" kg\nVitesse Max ==>  "+vitesseMaxF1()+" km/h";
-        }         
+    }
+
+    public double vitesseMaxF1() {
+        double vitesseMaxF1 = this.moteur.getVitesseTheorique() - (this.poids * 0.05);
+        return vitesseMaxF1;
+    }
+
+    public String toString() {
+        return "------------------------VOITURE DE COURSE---------------------------------------------\nMarque ==> "
+                + marqueVoiture + "\nPoids ==> " + poids + " kg\nVitesse Max ==>  " + vitesseMaxF1() + " km/h";
+    }
 }
