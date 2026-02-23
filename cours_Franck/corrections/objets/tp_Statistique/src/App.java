ac1c5c39d801fb0c95cb2d4aadb09818eedfc41e
Jims42 <jimmycoco.42@outlook.fr>
2026-02-23 15:57:11 +0100

    tp

--- cours_Franck/corrections/objets/tp_Statistique/src/App.java
+++ cours_Franck/corrections/objets/tp_Statistique/src/App.java
@@ -5,16 +5,42 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner clavier = new Scanner(System.in);
-        String data = "2100, 2150, 2200, 2250, 2300, 2350, 2400, 2450, 2500, 2550, 2600, 2650, 2700, 2750, 2800, 2900, 2950, 2950, 3000, 3000, 3000, 3000,3100, 3100, 3150, 3150, 3200, 3200, 3250, 3250, 3300, 3300, 3350, 3350, 3400, 3400, 3450, 3500, 3550, 3600, 3650, 3700, 3750, 3800, 3850, 3900, 3900, 3950, 3950, 4000,4100, 4150, 4200, 4250, 4300, 4350, 4400, 4450, 4500, 4550, 4600, 4650, 4700, 4750, 4800, 4850, 4900, 4950, 5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5500,5600, 5700, 5800, 5900, 6000, 6100, 6200, 6300, 6400, 6500, 6700, 6900, 7100, 7400, 7700, 8000, 8300, 8700, 9100, 9500";
-        String[] tabData=data.split(", ");
-        ArrayList<Double> dataSalaire=new ArrayList<Double>();
+        String data = "2100, 2150, 2200, 2250, 2300, 2350, 2400, 2450, 2500, 2550, 2600, 2650, 2700, 2750, 2800, 2900, 2950, 2950, 3000, 3000, 3000, 3000, 3100, 3100, 3150, 3150, 3200, 3200, 3250, 3250, 3300, 3300, 3350, 3350, 3400, 3400, 3450, 3500, 3550, 3600, 3650, 3700, 3750, 3800, 3850, 3900, 3900, 3950, 3950, 4000, 4100, 4150, 4200, 4250, 4300, 4350, 4400, 4450, 4500, 4550, 4600, 4650, 4700, 4750, 4800, 4850, 4900, 4950, 5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5500, 5600, 5700, 5800, 5900, 6000, 6100, 6200, 6300, 6400, 6500, 6700, 6900, 7100, 7400, 7700, 8000, 8300, 8700, 9100, 9500";
+        String[] tabData = data.split(", ");
+        ArrayList<Double> dataSalaire = new ArrayList<Double>();
         // Collections.addAll(dataSalaire, tabData);
         for (String valeur : tabData) {
             dataSalaire.add(Double.parseDouble(valeur));
-                  }
-                  for (double element : dataSalaire) {
-                    System.out.print(element+", ");
-                  }
+        }
+        for (double element : dataSalaire) {
+            System.out.print(element + ", ");
+        }
+        System.out.println();
+        Statistique objetStatistique = new Statistique(dataSalaire);
+        System.out.println("Affichage de la moyenne de l'echantillon de salaire des metiers de l'informatique");
+        double moyenne=objetStatistique.calculMoyenne();
+        System.out.println("Salaire moyen de l'echantillon : "+moyenne+" euros");
+        System.out.println("Affichage de l'ecart type l'echantillons de salaire des metiers de l'informatique : ");
+        double ecart=objetStatistique.calculEquartType();
+        System.out.println( "Ecart type moyenne + ou - :"+ecart+" euros");
+         System.out.println("Affichage de la valeure medianne l'echantillons de salaire des metiers de l'informatique : ");
+         System.out.println("Salaire median : "+objetStatistique.calculMedianne()+" euros");
+         System.out.println("Affichage des quartil  de salaire des metiers de l'informatique : ");
+        //  objetStatistique.calculQuartile();
+    double [] tabQuartil=new double[3];
+        tabQuartil=objetStatistique.calculQuartile();
+        for (int i = 1; i < tabQuartil.length+1; i++) {
+            
+        
+        for (double resultat : tabQuartil) {
+    
+           
+        }
+    }
+
+
+
+        clavier.close();
 
     }
 }
--- cours_Franck/corrections/objets/tp_Statistique/src/Statistique.java
+++ cours_Franck/corrections/objets/tp_Statistique/src/Statistique.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Statistique {
     private ArrayList<Double> myData = new ArrayList<Double>();
@@ -6,10 +8,15 @@ public class Statistique {
 
     public Statistique(ArrayList<Double> _monTab) {
         this.myData.addAll(_monTab);
+         // tri croissant
+        Collections.sort(this.myData);
+        // methode >= java 8
+        // myData.sort(Comparator.naturalOrder()); tri croissant
+        // myData.sort(Comparator.reverseOrder()); tri decroissant
         this.nbElement = myData.size();
     }
-    public ArrayList<Double> getMyData(){return myData;}
-    public void setMyData(ArrayList<Double> _newVal){
+    public ArrayList<Double> getmyData(){return myData;}
+    public void setmyData(ArrayList<Double> _newVal){
         this.myData=_newVal;
     }
     public double calculMoyenne(){
@@ -21,7 +28,7 @@ public class Statistique {
         return calculArrondi(resultat, 2);
     }
     private double calculArrondi(double _nombre,int _decimal){
-        return (double)(Math.pow(10, _decimal)*_nombre+0.5)/Math.pow(10, _decimal);
+        return (double)((long)(Math.pow(10, _decimal)*_nombre+0.5))/Math.pow(10, _decimal);
     }
     public double calculEquartType(){
         double temp=0;
@@ -34,5 +41,39 @@ public class Statistique {
         res=Math.sqrt(temp);
         return calculArrondi(res, 2);
     }
+    public double calculMedianne(){
+               if(this.nbElement%2==0){
+                double medianne=(myData.get(this.nbElement/2)+myData.get((this.nbElement/2)-1))/2;
+                return medianne;
+        }
+        return myData.get(this.nbElement/2);
+    }
+    public double [] calculquartil(){
+        double[] quartil=new double[3];
+         if (nbElement % 4 == 0) {
+   quartil[0] = myData.get((nbElement / 4));
+  } else {
+   double moyenneSal = (myData.get((int) (Math.floor(this.nbElement / 4)))
+     + myData.get((int) Math.ceil(this.nbElement / 4))) / 2;
+   quartil[0] = moyenneSal;
+  }
+  quartil[1] = this.calculMedianne();
+  if (this.nbElement * 3 % 4 == 0) {
 
-}
+   quartil[2] = myData.get((nbElement * 3 / 4));
+
+  } else {
+   double moyenneSal2 = (myData.get((int) (Math.floor(this.nbElement * 3 / 4)))
+     + myData.get((int) Math.ceil(this.nbElement * 3 / 4))) / 2;
+
+   quartil[2] = moyenneSal2;
+
+  }
+         return quartil;
+
+    }
+     
+		
+	}  
+
+
