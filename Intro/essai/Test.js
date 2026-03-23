2dabe9cfd0b96ce3ec81d46ac79e93c6b41b9fe6
Jims42 <jimmycoco.42@outlook.fr>
2026-03-23 14:26:27 +0100

    html

--- Intro/essai/Test.js
+++ Intro/essai/Test.js
@@ -6,11 +6,23 @@
     //   const btn5 = document.getElementById("btn5");
     //   const btn6 = document.getElementById("btn6");
       
-      btn1.onclick = () => document.body.style.backgroundColor = "blue";
-      btn2.onclick = () => document.body.style.backgroundColor = "violet";
-      btn3.onclick = () => document.body.style.backgroundColor = "green";
-      btn4.onclick = () => document.body.style.backgroundColor = "yellow";
-      btn5.onclick = () => document.body.style.backgroundColor = "orange";
-      btn6.onclick = () => document.body.style.backgroundColor = "red";
+      // btn1.onclick = () => document.body.style.backgroundColor = "blue";
+      // btn2.onclick = () => document.body.style.backgroundColor = "violet";
+      // btn3.onclick = () => document.body.style.backgroundColor = "green";
+      // btn4.onclick = () => document.body.style.backgroundColor = "yellow";
+      // btn5.onclick = () => document.body.style.backgroundColor = "orange";
+      // btn6.onclick = () => document.body.style.backgroundColor = "red";
       
-    
+
+      const btn1 = document.querySelector("#btn1");
+      btn1.addEventListener('click',function(){document.body.style.backgroundColor="blue"});
+    const btn2 = document.querySelector("#btn2");
+      btn2.addEventListener('click',function(){document.body.style.backgroundColor="violet"});
+      const btn3 = document.querySelector("#btn3");
+      btn3.addEventListener('click',function(){document.body.style.backgroundColor="green"});
+      const btn4 = document.querySelector("#btn4");
+      btn4.addEventListener('click',function(){document.body.style.backgroundColor="yellow"});
+      const btn5 = document.querySelector("#btn5");
+      btn5.addEventListener('click',function(){document.body.style.backgroundColor="orange"});
+      const btn6 = document.querySelector("#btn6");
+      btn6.addEventListener('click',function(){document.body.style.backgroundColor="red"});
