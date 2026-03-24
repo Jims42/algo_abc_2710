3cb0a71b3194d1d4737f4b9b24f81ad27e15c1a7
Jims42 <jimmycoco.42@outlook.fr>
2026-03-24 06:57:07 +0100

    tt

--- cours_Franck/exercises/Exo01_4.java
+++ cours_Franck/exercises/Exo01_4.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
+
 public class Exo01_4 {
     public static void main(String[] args) {
-        
-        Scanner clavier = new Scanner(System.in);
-        double S;  //somme
-        double i=0.4;  //interet
-                
-        //System.out.println("Lire la somme S placée sur un compte, l'intérêt I offert par la banque et le nombre N d'années de placement de la somme s.");
-        System.out.print("Indiquez la somme : ");S=clavier.nextDouble();
-        System.out.print("indiquez le nombre d'années : ");int N = clavier.nextInt();
-        double interet=S*(1+N*i);
-        System.out.printf("avec interet simple : %.2f  \n",interet);
-        double credit=S*Math.pow((1+i),N);
-        System.out.printf("avec un interet composé : %.2f \n",credit );
-        clavier.close();
 
+        Scanner clavier = new Scanner(System.in);
+        double S; // somme
+        double i = 0.4; // interet
+
+        // System.out.println("Lire la somme S placée sur un compte, l'intérêt I offert
+        // par la banque et le nombre N d'années de placement de la somme s.");
+        System.out.print("Indiquez la somme : ");
+        S = clavier.nextDouble();
+        System.out.print("indiquez le nombre d'années : ");
+        int N = clavier.nextInt();
+        double interet = S * (1 + N * i);
+        System.out.printf("avec interet simple : %.2f  \n", interet);
+        double credit = S * Math.pow((1 + i), N);
+        System.out.printf("avec un interet composé : %.2f \n", credit);
+        clavier.close();
 
     }
 }
